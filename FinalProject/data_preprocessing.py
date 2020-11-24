24b116f5bc01687dafc496c8997fe87ec05164ff
morrisChen <morris7307@gmail.com>
2020-11-25 01:55:59 +0800

    final project preprocess

--- FinalProject/data_preprocessing.py
+++ FinalProject/data_preprocessing.py
@@ -8,6 +8,7 @@ import pandas as pd
 
 training_data = pd.read_json('yelp_training_review.json',lines=True)
 training_data = training_data.drop(columns=['votes','date','type'])
-#%%
-user_list = pd.read_json('yelp_training_user.json',lines=True)['user_id']
-business_list = pd.read_json('yelp_training_business.json',lines=True)['business_id']
+#user_list = list(pd.read_json('yelp_training_user.json',lines=True)['user_id'])
+#business_list = list(pd.read_json('yelp_training_business.json',lines=True)['business_id'])
+training_data = pd.pivot_table(training_data, values='stars', index=['user_id'], columns=['business_id'], aggfunc='mean')
+training_data.fillna(0,inplace=True)
