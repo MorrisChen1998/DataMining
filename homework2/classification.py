d10f2df50be7b2b7037ef7a797117b27ef424648
Morris Chen <morris7307@gmail.com>
2020-12-01 17:48:35 +0800

    hw2 experiment finished

--- homework2/classification.py
+++ homework2/classification.py
@@ -7,7 +7,6 @@ Created on Wed Nov 25 01:59:48 2020
 import pandas as pd
 from sklearn import preprocessing
 from sklearn import tree#DecisionTreeClassifier
-from sklearn import ensemble#ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
 
 train_data = pd.read_csv('classfication/csv/train.csv').values
@@ -15,11 +14,16 @@ test_data = pd.read_csv('classfication/csv/test.csv').values
 
 #%%
 data = preprocessing.scale(train_data[:,:-1])
-X_train, X_test, y_train, y_test = train_test_split(data, train_data[:,-1], test_size=0.25)
-clf = ensemble.ExtraTreesClassifier(criterion='entropy',class_weight='balanced')
-#clf = tree.DecisionTreeClassifier(criterion='entropy',class_weight='balanced')
+X_train, X_test, y_train, y_test = train_test_split(
+    data, train_data[:,-1], test_size=0.25,random_state=0)
+clf = tree.DecisionTreeClassifier(
+    criterion='entropy',
+    class_weight='balanced',
+    random_state=0,
+    min_samples_split=5,
+    max_depth=15)
 clf = clf.fit(X_train, y_train)
-validation = clf.score(X_test, y_test)
+validation=clf.score(X_test, y_test)
 
 #%%
 from print_answer import printOutAnswer
--- homework2/clustering.py
+++ homework2/clustering.py
@@ -9,7 +9,7 @@ from sklearn import metrics
 from sklearn import cluster
 from sklearn import preprocessing
 #
-data_origin = preprocessing.scale(pd.read_csv('Clustering/csv/origin/cluster_train.csv').values)
+data_origin = (pd.read_csv('Clustering/csv/origin/cluster_train.csv').values)
 
 data_5f = (pd.read_csv('Clustering/csv/lsa/LSA_5Feature.csv').values)
 data_10f = (pd.read_csv('Clustering/csv/lsa/LSA_10Feature.csv').values)
@@ -17,18 +17,27 @@ data_20f = (pd.read_csv('Clustering/csv/lsa/LSA_20Feature.csv').values)
 data_40f = (pd.read_csv('Clustering/csv/lsa/LSA_40Feature.csv').values)
 
 #%%
-clustering_origin = cluster.KMeans(n_clusters=6).fit(data_origin)
-clustering_5f = cluster.KMeans(n_clusters=6).fit(data_5f)
-clustering_10f = cluster.KMeans(n_clusters=6).fit(data_10f)
-clustering_20f = cluster.KMeans(n_clusters=6).fit(data_20f)
-clustering_40f = cluster.KMeans(n_clusters=6).fit(data_40f)
+def clustering(data):
+    return cluster.KMeans(n_clusters=6).fit(data)
+def silhouetteScore(data,label):
+    return metrics.silhouette_score(data, label, metric='euclidean')
+    
+clustering_origin = clustering(data_origin)
+clustering_5f = clustering(data_5f)
+clustering_10f = clustering(data_10f)
+clustering_20f = clustering(data_20f)
+clustering_40f = clustering(data_40f)
 
-#%
-score_origin = metrics.silhouette_score(data_origin, clustering_origin.labels_, metric='euclidean')
-score_5f = metrics.silhouette_score(data_5f, clustering_5f.labels_, metric='euclidean')
-score_10f = metrics.silhouette_score(data_10f, clustering_10f.labels_, metric='euclidean')
-score_20f = metrics.silhouette_score(data_20f, clustering_20f.labels_, metric='euclidean')
-score_40f = metrics.silhouette_score(data_40f, clustering_40f.labels_, metric='euclidean')
+score_origin = silhouetteScore(data_origin,clustering_origin.labels_)
+score_5f = silhouetteScore(data_5f, clustering_5f.labels_)
+score_10f = silhouetteScore(data_10f, clustering_10f.labels_)
+score_20f = silhouetteScore(data_20f, clustering_20f.labels_)
+score_40f = silhouetteScore(data_40f, clustering_40f.labels_)
+print('5f=%.3f'%score_5f)
+print('10f=%.3f'%score_10f)
+print('20f=%.3f'%score_20f)
+print('40f=%.3f'%score_40f)
+print('origin(60f)=%.3f'%score_origin)
 
 #%%
 from print_answer import printOutAnswer
